e2a8fbecdc24fe5e76fe6fdec659f6a3ad510671
Franco Rodriguez <francojmprogramacion@gmail.com>
2022-07-08 08:33:21 -0300

    edicion de avatar

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -102,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# ruta a media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
